6ad4838e7bcdcd4b89ac2c71f13ba9e3eace7f56
rodrigo.mardel <rodrigo.mardel.daw@gmail.com>
2025-08-15 18:03:41 +0200

    [FEAT] - Custom hook for input calculator value

--- 03-iosCalculator/src/presentation/components/CalculatorButton.tsx
+++ 03-iosCalculator/src/presentation/components/CalculatorButton.tsx
@@ -6,17 +6,21 @@ interface CalculatorButtonProps {
   label: string,
   color?: string,
   doubleSize?: boolean,
-  blackText?: boolean
+  blackText?: boolean,
+  onPress: () => void
 }
 
 export const CalculatorButton = ({
   label,
   color = colors.darkGray,
   doubleSize = false,
-  blackText = false
+  blackText = false,
+  onPress
   }: CalculatorButtonProps) => {
   return (
-    <Pressable style={({pressed}) => ({
+    <Pressable 
+    onPress={onPress}
+    style={({pressed}) => ({
       ...styles.button,
       backgroundColor: color,
       width: (doubleSize) ? 180 : 80,
--- 03-iosCalculator/src/presentation/screens/CalculatorScreen.tsx
+++ 03-iosCalculator/src/presentation/screens/CalculatorScreen.tsx
@@ -2,47 +2,54 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import { colors, styles } from '../../config/theme/app-theme'
 import { CalculatorButton } from '../components'
+import { useCalculator } from '../hooks/useCalculator'
 
 export const CalculatorScreen = () => {
+
+  const {number, buildNumber} = useCalculator();
+
   return (
     <View style={styles.calculatorContainer}>
       <View style= {{paddingHorizontal: 30, paddingBottom: 20}}>
-      <Text style={styles.mainResult}>1500</Text>
+      <Text 
+      adjustsFontSizeToFit
+      numberOfLines={1}
+      style={styles.mainResult}>{number}</Text>
       <Text style={styles.subResult}>15</Text>
       </View>
 
       <View style={styles.row}>
-        <CalculatorButton label="C" color={colors.lightGray} blackText/>
-        <CalculatorButton label="+/-" color={colors.lightGray} blackText/>
-        <CalculatorButton label="del" color={colors.lightGray} blackText/>
-        <CalculatorButton label="/" color={colors.orange}/> 
+        <CalculatorButton onPress={() => console.log('C')} label="C" color={colors.lightGray} blackText/>
+        <CalculatorButton onPress={() => console.log('+/-')} label="+/-" color={colors.lightGray} blackText/>
+        <CalculatorButton onPress={() => console.log('del')} label="del" color={colors.lightGray} blackText/>
+        <CalculatorButton onPress={() => console.log('/')} label="/" color={colors.orange}/> 
       </View>
 
       <View style={styles.row}>
-        <CalculatorButton label="7" color={colors.darkGray}/>
-        <CalculatorButton label="8" color={colors.darkGray}/>
-        <CalculatorButton label="9" color={colors.darkGray}/>
-        <CalculatorButton label="x" color={colors.orange}/> 
+        <CalculatorButton onPress={() => buildNumber('7')} label="7" color={colors.darkGray}/>
+        <CalculatorButton onPress={() => buildNumber('8')} label="8" color={colors.darkGray}/>
+        <CalculatorButton onPress={() => buildNumber('9')} label="9" color={colors.darkGray}/>
+        <CalculatorButton onPress={() => console.log('x')} label="x" color={colors.orange}/> 
       </View>
 
       <View style={styles.row}>
-        <CalculatorButton label="4" color={colors.darkGray}/>
-        <CalculatorButton label="5" color={colors.darkGray}/>
-        <CalculatorButton label="6" color={colors.darkGray}/>
-        <CalculatorButton label="-" color={colors.orange}/> 
+        <CalculatorButton onPress={() => buildNumber('4')} label="4" color={colors.darkGray}/>
+        <CalculatorButton onPress={() => buildNumber('5')} label="5" color={colors.darkGray}/>
+        <CalculatorButton onPress={() => buildNumber('6')} label="6" color={colors.darkGray}/>
+        <CalculatorButton onPress={() => console.log('-')} label="-" color={colors.orange}/> 
       </View>   
 
       <View style={styles.row}>
-        <CalculatorButton label="1" color={colors.darkGray}/>
-        <CalculatorButton label="2" color={colors.darkGray}/>
-        <CalculatorButton label="3" color={colors.darkGray}/>
-        <CalculatorButton label="+" color={colors.orange}/> 
+        <CalculatorButton onPress={() => buildNumber('1')} label="1" color={colors.darkGray}/>
+        <CalculatorButton onPress={() => buildNumber('2')} label="2" color={colors.darkGray}/>
+        <CalculatorButton onPress={() => buildNumber('3')} label="3" color={colors.darkGray}/>
+        <CalculatorButton onPress={() => console.log('+')} label="+" color={colors.orange}/> 
       </View> 
 
       <View style={styles.row}>
-        <CalculatorButton label="0" color={colors.darkGray} doubleSize/>
-        <CalculatorButton label="." color={colors.darkGray}/>
-        <CalculatorButton label="=" color={colors.orange}/> 
+        <CalculatorButton onPress={() => buildNumber('0')} label="0" color={colors.darkGray} doubleSize/>
+        <CalculatorButton onPress={() => buildNumber('.')} label="." color={colors.darkGray}/>
+        <CalculatorButton onPress={() => console.log('=')} label="=" color={colors.orange}/> 
       </View>   
     </View>
   )
