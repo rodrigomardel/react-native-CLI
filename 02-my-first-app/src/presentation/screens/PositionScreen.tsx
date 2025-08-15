9a6d369cabb2adb40a1d1d1b302ecdb63e33f272
rodrigo.mardel <rodrigo.mardel.daw@gmail.com>
2025-08-15 08:51:59 +0200

    [FEAT] - Posición absoluta

--- 02-my-first-app/src/presentation/screens/PositionScreen.tsx
+++ 02-my-first-app/src/presentation/screens/PositionScreen.tsx
@@ -4,9 +4,9 @@ import { View, Text, StyleSheet } from 'react-native';
 export const PositionScreen = () => {
   return (
     <View style={styles.container}>
+      <View style={styles.greenBox}/>
       <View style={styles.purpleBox}/>
       <View style={styles.orangeBox}/>
-
     </View>
   );
 };
@@ -14,9 +14,11 @@ export const PositionScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    // height: 400,
+    // width: 400,
     backgroundColor: 'lightblue',
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
   },
   purpleBox: {
     backgroundColor: 'purple',
@@ -24,8 +26,9 @@ const styles = StyleSheet.create({
     height: 100,
     borderWidth: 10,
     borderColor: 'white',
-    position: 'relative',
-    top: -50,
+    position: 'absolute',
+    // top: 40,
+    bottom: 0,
   },
   orangeBox: {
     backgroundColor: 'orange',
@@ -33,7 +36,20 @@ const styles = StyleSheet.create({
     height: 100,
     borderWidth: 10,
     borderColor: 'white',
-    top: 0,
-    left: -100,
+    position: 'absolute',
+    right: 0,
+  },
+  greenBox: {
+    backgroundColor: 'green',
+    // width: 100,
+    // height: 100,
+    borderWidth: 10,
+    borderColor: 'white',
+    // position: 'absolute',
+    // bottom: 0,
+    // right: 0,
+    // top: 0,
+    // left: 0,
+    ...StyleSheet.absoluteFillObject,
   },
 });
