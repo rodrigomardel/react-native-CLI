2db8a96301ae6f16e5234dce9d7fb261c85f7944
rodrigo.mardel <rodrigo.mardel.daw@gmail.com>
2025-08-15 11:18:08 +0200

    [FEAT] - Init project calculator | React Native 0.81.0

---
+++ 03-ios-calculator/src/App.tsx
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { StatusBar, View, Text } from 'react-native';
+
+function App() {
+
+  return (
+    <View>
+      <StatusBar barStyle={'light-content'} backgroundColor={'black'} />
+      <Text>Hola Mundo</Text>
+    </View>
+  );
+}
+
+export default App;
