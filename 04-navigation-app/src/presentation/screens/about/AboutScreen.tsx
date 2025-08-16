fe98cf3671166011f737611d132f498cda1694e3
rodrigo.mardel <rodrigo.mardel.daw@gmail.com>
2025-08-16 08:26:30 +0200

    [FEAT] - File structure

---
+++ 04-navigation-app/src/presentation/screens/about/AboutScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export const AboutScreen = () => {
+  return (
+    <View>
+      <Text>AboutScreen</Text>
+    </View>
+  )
+}
