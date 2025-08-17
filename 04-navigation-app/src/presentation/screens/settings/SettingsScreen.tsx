86da289d728f0241dd5c14f2b9333ce048fc8d27
rodrigo.mardel <rodrigo.mardel.daw@gmail.com>
2025-08-17 08:16:05 +0200

    [FEAT] - Navigation goBack & popToTop

--- 04-navigation-app/src/presentation/screens/settings/SettingsScreen.tsx
+++ 04-navigation-app/src/presentation/screens/settings/SettingsScreen.tsx
@@ -1,10 +1,24 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { globalStyles } from '../../theme/theme'
+import { PrimaryButton } from '../../components'
+import { NavigationProp, StackActions, useNavigation } from '@react-navigation/native'
+import { RootStackParams } from '../../routes/StackNavigator'
 
 export const SettingsScreen = () => {
+  const navigation = useNavigation<NavigationProp<RootStackParams>>();
+
   return (
-    <View>
-      <Text>SettingsScreen</Text>
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.title}>SettingsScreen</Text>
+      <PrimaryButton
+        label='Regresar'
+        onPress={() => navigation.goBack()}
+      />
+      <PrimaryButton
+        label='Go to Home'
+        onPress={() => navigation.dispatch(StackActions.popToTop())}
+      />
     </View>
   )
 }
