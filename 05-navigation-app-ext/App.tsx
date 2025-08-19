550dcbd478e49f3c5ec3aa34b0945bdffd4f7166
rodrigo.mardel <rodrigo.mardel.daw@gmail.com>
2025-08-19 07:43:54 +0200

    [CONFIG] - react-native-paper & react-native-safe-area-context

--- 05-navigation-app-ext/App.tsx
+++ 05-navigation-app-ext/App.tsx
@@ -5,13 +5,17 @@
 
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { AppRegistry } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
 import { AppNavigator } from './src/presentation/navigation/AppNavigator';
 
 function App(): React.JSX.Element {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <AppNavigator />
+      <PaperProvider>
+        <AppNavigator />
+      </PaperProvider>
     </>
   );
 }
