469b1cc84f22b8c6c4dccd0d7a1445c4aed12915
rodrigo.mardel <rodrigo.mardel@outlook.es>
2025-08-21 22:12:45 +0200

    [FEAT] - Rename project

--- 06-context-app/android/app/src/main/java/com/contextapp/MainApplication.kt
+++ 06-context-app/android/app/src/main/java/com/contextapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.contextappzustand
+package com.contextapp
 
 import android.app.Application
 import com.facebook.react.PackageList
--- 06-context-app/android/app/src/main/java/com/contextappzustand/MainActivity.kt
+++ 06-context-app/android/app/src/main/java/com/contextappzustand/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "06-context-app-zustand"
+  override fun getMainComponentName(): String = "06-context-app"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
